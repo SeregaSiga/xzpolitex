2e6e7bcfd0f1398ca5f57a8a8a239efa4b313995
SeregaSiga <90831158+SeregaSiga@users.noreply.github.com>
2021-09-23 12:04:05 +0300

    Add files via upload

---
+++ serega.cpp
@@ -0,0 +1,43 @@
+﻿
+#include <pch.h>
+#include <math.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	int b;
+	int s;
+	
+	cout <<'a' <<'b' << endl;
+	cin >> a >> b;
+	s = a * b;
+	cout << 's' << endl;
+	cout << s << endl;
+	double L;
+	double c;
+	double d;
+	cout << 'd' << endl;
+	cin >> d;
+		L =d * 3.14;
+		cout << 'L' << endl;
+		cout << L << endl;
+		c = (a + b) / 2;
+		int x, y;
+		cout << 'x' << 'y' << endl;
+		cin >> x >> y;
+		cout << 'sum' << ' ' << 'razn' << ' ' << 'pr' << ' ' << 'ch'<< endl;
+		cout << x + y << ' ' << x - y << ' ' << x * y << ' ' << (x*x) / (y*y)<<endl;
+		int m;
+		int l;
+		cout << 'm' << 'l' << endl;
+		cin >> m >> l;
+		cout << 'sum' << ' ' << 'razn' << ' ' << 'pr' << ' ' << 'mod' << endl;
+		cout << m + l << ' ' << m - l << ' ' << m * l << ' ' << abs(m) / abs(l);
+
+		
+
+}
+
